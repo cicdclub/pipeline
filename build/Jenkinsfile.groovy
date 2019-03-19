9c704bc670fa314ce997b712af3b00d09ac9e761
Juan Manuel Carrillo Moreno <inetshell@gmail.com>
2019-03-18 21:59:47 -0600

    Test with variables

--- build/Jenkinsfile.groovy
+++ build/Jenkinsfile.groovy
@@ -2,7 +2,7 @@
 def call(){
     pipeline {
         environment {
-            config_file = load 'pipeline/config.groovy'
+            config_file='pipeline/config.groovy'
             DOCKER_CRED = ''
             DOCKER_REPO = ''
             DOCKER_REG = ''
@@ -13,6 +13,9 @@ def call(){
         stages {
             stage('SETUP') {
                 steps {
+                    script{
+                        config_file = load "$config_file"
+                    }
                     echo "Login to ${config_file.DOCKER_CRED} docker registry..."
                     withCredentials([[$class: 'UsernamePasswordMultiBinding',
                         credentialsId: "${config_file.DOCKER_CRED}",
